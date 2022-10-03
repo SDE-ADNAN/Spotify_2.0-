936fb465ddb0f9d5879ae27c2be87d0651aab66a
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-04 00:58:33 +0530

    corrected the issue of not loading rerenders

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -28,7 +28,7 @@ const Discover = () => {
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} value={song} song={song} i={i} />
+          <SongCard key={song.key} song={song} i={i}></SongCard>
         ))}
       </div>
     </div>
