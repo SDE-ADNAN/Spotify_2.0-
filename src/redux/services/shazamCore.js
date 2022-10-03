8c8418504532968cca91e48e2a95cfa998164fe2
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-03 22:44:47 +0530

    added the required code for shazamCore.js for calling the shazam api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -15,4 +15,19 @@ fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
 
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: () => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "6f0b734d66msh4977546389c9faep186980jsn9c4748d42daa"
+      );
+      // headers.set("X-RapidAPI-Host","shazam-core.p.rapidapi.com"),
+    },
+    endpoints: (builder) => ({
+      getTopCharts: builder.query({ query: "/charts/world" }),
+    }),
+  }),
 });
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
