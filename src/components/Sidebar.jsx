61925b049f80b2dc241d649282b58e2c0e743765
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-05 20:29:05 +0530

    added the navLink component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,12 +6,30 @@ import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
 
+const NavLinks = ({ handleClick }) => {
+  return (
+    <div className="mt-10 ">
+      {links.map((item) => (
+        <NavLink
+          key={item.name}
+          to={item.path}
+          className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+          onClick={() => handleClick && handleClick()}
+        >
+          <item.icon className="w-6 h-6 mr-2"></item.icon>
+          {item.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain"></img>
+        <NavLinks />
       </div>
     </>
   );
