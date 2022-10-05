0afe535ec8ad3cfe57e9340de0085314bac65e39
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-05 20:43:57 +0530

    added the mobile sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { HiOutlineMenu } from "react-icons/hi";
 import { RiCloseLine } from "react-icons/ri";
 
 import { logo } from "../assets";
@@ -30,6 +31,14 @@ const Sidebar = () => {
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain"></img>
         <NavLinks />
+      </div>
+      {/* //MobileMenu  */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-6 h-6 text-white mr-2" />
+        ) : (
+          <HiOutlineMenu className="w-6 h-6 text-white mr-2" />
+        )}
       </div>
     </>
   );
