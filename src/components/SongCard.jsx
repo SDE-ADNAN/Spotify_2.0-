dd2e3bd0b0a9d579815add2a5147d37c3f85985e
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-10-06 23:10:55 +0530

    completedd the SongCard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
@@ -6,35 +7,43 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   const dispatch = useDispatch();
+
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
+
   const handlePlayClick = () => {
-    dispatch(setActiveSong({song,data,i}));
+    dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
+
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
-              ? "flex bg-black bg-opacity-70 "
+              ? "flex bg-black bg-opacity-70"
               : "hidden"
           }`}
         >
           <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
             song={song}
             handlePause={handlePauseClick}
             handlePlay={handlePlayClick}
-            isPlaying={isPlaying}
-            activeSong={activeSong}
-          ></PlayPause>
+          />
         </div>
-        <img alt="song_img" src={song.images?.coverart}></img>
+        <img
+          alt="song_img"
+          src={song.images?.coverart}
+          className="w-full h-full rounded-lg"
+        />
       </div>
+
       <div className="mt-4 flex flex-col">
-        <p className="font-semibold  text-lg text-white truncate">
+        <p className="font-semibold text-lg text-white truncate">
           <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
         <p className="text-sm truncate text-gray-300 mt-1">
@@ -42,7 +51,7 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
             to={
               song.artists
                 ? `/artists/${song?.artists[0]?.adamid}`
-                : "top-artists"
+                : "/top-artists"
             }
           >
             {song.subtitle}
